14d9f621f07e0e806ddecfe0cfe0ad3201ee904d
Anurag Ajwani <anurag.ajwani@gmail.com>
2022-01-15 14:37:18 +0000

    Initial commit

--- UIViewControllerSwiftUIPreview/ViewController.swift
+++ UIViewControllerSwiftUIPreview/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    private let label: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "hello"
+        view.textColor = .black
+        return view
+    }()
+    
+    override func loadView() {
+        self.view = UIView()
+        self.view.backgroundColor = .white
+        self.setupSubviews()
     }
 
-
+    private func setupSubviews() {
+        self.setupLabel()
+    }
+    
+    private func setupLabel() {
+        self.view.addSubview(self.label)
+        NSLayoutConstraint.activate([
+            self.label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        ])
+    }
 }
+
+
 
